afc5c4ad5e709febb09241238e69a991d51c39f6
Shruti Shinde <shrutishinde145@gmail.com>
2023-10-19 15:05:45 +0530

    add all changes

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,15 +16,15 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/2.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "This is very beautiful";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am currently MLSA of my college and also very much interested in technology";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -32,10 +32,10 @@ const description =
  */
 const skillsList = [
   "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
+  "Coder",
+  "Canva Desing",
+  "Gamer",
+  "UI/UX",
   "Graphic design",
 ];
 
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "This is my portfolio website";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/3.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Amazing flower";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Discord CLone",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Made using HTML and Tailwind CSS ",
+    url: "https://shrutshinde.github.io/Discord/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Weather App",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Made with HTML and CSS and used OpenWeather API",
+    url: "https://shrutshinde.github.io/WeatherApp/",
   },
   {
-    title: "My Resume Site",
+    title: "Password Generator",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "It creates password of any length",
+    url: "https://shrutshinde.github.io/Password_Generator/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Github",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "My Github Id",
+    url: "https://github.com/Shrutshinde",
   },
 ];
 
